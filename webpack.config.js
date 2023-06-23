056a62933591478ced4880bb609bfdc8abafec72
Alejae1998 <alejagr1998@gmail.com>
2023-06-23 11:45:27 -0700

    more npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,47 @@
 const path = require('path');
-// below  const path = require('path');, add this line:
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-
 module.exports = {
-  entry: './src/index.js', // this indicates the main js file that everything will be bundled into
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js', // this is the file name for the bundled output
-    path: path.resolve(__dirname, 'dist'), // this is the directory for the bundled output, in this case it's 'dist'
-    devtool: 'eval-source-map',
-    devServer: {
-      contentBase: './dist',
-    },
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'eval-source-map',
   plugins: [
     new ESLintPlugin(),
-
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Project', // this line will be the title that shows up in your browser tab
-      template: './src/index.html', // everything will get injected into this file
+      title: 'Project',
+      template: './src/index.html',
       inject: 'body',
     }),
   ],
-  // this gets added right after the 'output' object, still within the `module.exports` object
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
       },
     ],
   },
